0c0b0de11a13f0bd63e956c269e851a90a54695b
Pahomch1k <P.lupashko.main@gmail.com>
2024-05-20 20:51:06 +0300

    local image support

---
+++ AirbnbDiploma/Controllers/ImagesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirbnbDiploma.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ImagesController : ControllerBase
+{
+    [HttpGet("{imageName}")]
+    public ActionResult GetFile(string imageName)
+    {
+        try
+        {
+            var currentDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            FileStream stream = new($"{currentDir}/images/{imageName}.jpg", FileMode.Open);
+            FileStreamResult result = new(stream, "image/jpeg");
+            return result;
+        }
+        catch (IOException)
+        {
+            return NotFound();
+        }
+    }
+}
