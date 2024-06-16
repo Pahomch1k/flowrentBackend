03867e4b4261b6dc4e8eace97b59a0eccca8d5ef
Pahomch1k <P.lupashko.main@gmail.com>
2024-06-16 20:46:01 +0300

    fixed docker-compose, fixed image saving

--- AirbnbDiploma.BLL/Services/ImageService/ImageService.cs
+++ AirbnbDiploma.BLL/Services/ImageService/ImageService.cs
@@ -7,6 +7,7 @@ public class ImageService : IImageService
     public async Task SaveImageAsync(string name, string contentBase64)
     {
         var currentDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        Directory.CreateDirectory($"{currentDir}/images");
         var bytes = Convert.FromBase64String(contentBase64);
         await File.WriteAllBytesAsync($"{currentDir}/images/{name}.jpg", bytes);
     }
